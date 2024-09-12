13f273e6349a10280e588df033eb97192b7b40f0
sakir <sakir@DESKTOP-N2FEHOR>
2024-09-12 21:05:29 +0700

    Добавьте файлы проекта.

---
+++ Pattern/Program.cs
@@ -0,0 +1,97 @@
+﻿using Pattern;
+bool run = true;
+/*
+Server instace = Server.Instance;
+Console.WriteLine("Выберите действие: " + '\n');
+
+
+while (run)
+{
+    Console.WriteLine("1) Добавить адрес\n" +
+        "2) Посмотреть все адресы https://\n" +
+        "3) Посмотреть все адресы http://\n" +
+        "4) Выход");
+
+    byte choose = byte.Parse(Console.ReadLine());
+    switch (choose)
+    {
+        case 1:
+            bool start = true;
+            Console.Clear();
+            Console.WriteLine("Введите символ q чтобы выйти");
+            while (start)
+            {
+                Console.WriteLine("Введите адрес: ");
+                string addres = Console.ReadLine();
+                if (addres.ToLower() == "q")
+                {
+                    Console.Clear();
+                    break;
+                }
+                if (eager_sign.AddServer(addres))
+                    Console.WriteLine("Успешно!");
+                else
+                    Console.WriteLine("Не удалось добавить!");
+                
+            }
+            break;
+
+        case 2:
+            instace.ShowAllHTTPS();
+            break;
+        case 3:
+            instace.ShowAllHTTP();
+            break;
+        case 4:
+            run = false;
+            break;
+    }
+}
+*/
+Server1 eager_sign = Server1.Instance;
+Console.WriteLine("Выберите действие: " + '\n');
+run = true;
+
+while (run)
+{
+    Console.WriteLine("1) Добавить адрес\n" +
+        "2) Посмотреть все адресы https://\n" +
+        "3) Посмотреть все адресы http://\n" +
+        "4) Выход");
+
+    byte choose = byte.Parse(Console.ReadLine());
+    switch (choose)
+    {
+        case 1:
+            bool start = true;
+            Console.Clear();
+            Console.WriteLine("Введите символ q чтобы выйти");
+            while (start)
+            {
+                Console.Write("Введите адрес: ");
+                string addres = Console.ReadLine();
+                if (addres.ToLower() == "q")
+                {
+                    Console.Clear();
+                    break;
+                }
+                if (eager_sign.AddServer(addres))
+                    Console.WriteLine("Успешно!");
+                else
+                    Console.WriteLine("Не удалось добавить!");
+                
+            }
+
+            break;
+
+        case 2:
+            eager_sign.ShowAllHTTPS();
+            break;
+        case 3:
+            eager_sign.ShowAllHTTP();
+            break;
+        case 4:
+            run = false;
+            break;
+    }
+}
---
+++ Pattern/Server.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pattern
+{
+    //Реализация Singleton Lazy
+    //Реализация Singleton Eager ниже
+    public class Server
+    {
+        private List<string> server_list = new List<string>();
+        private static Lazy<Server> instance = new Lazy<Server>(() => new Server());
+
+        private Server() { }
+
+        public static Server Instance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        public bool AddServer(string address)
+        {
+            if (!address.Contains("https") && !address.Contains("http"))
+            {
+                return false;
+            }
+            else
+            {
+                if (server_list.Contains(address))
+                {
+                    return false;
+                }
+                else
+                {
+                    server_list.Add(address);
+                    return true;
+                }
+            }
+        }
+
+        public void ShowAllHTTPS()
+        {
+            if (server_list.Count == 0)
+            {
+                Console.WriteLine("Список пуст");
+                return;
+            }
+            foreach (string server in server_list)
+            {
+                if (server.StartsWith("https"))
+                    Console.Write(server + " ");
+                else Console.Write("Отсувствует(ют) адрес(ы) с https");
+            }
+            Console.WriteLine('\n');
+        }
+
+        public void ShowAllHTTP()
+        {
+            if (server_list.Count == 0)
+            {
+                Console.WriteLine("Список пуст");
+                return;
+            }
+            foreach (string server in server_list)
+            {
+                if (server.StartsWith("http") && !server.StartsWith("https"))
+                    Console.Write(server + " ");
+                else
+                {
+                    Console.Write("Отсувствует(ют) адрес(ы) с http");
+                }
+            }
+            Console.WriteLine('\n');
+        }
+    }
+    
+    public class Server1
+    {
+        private List<string> server_list = new List<string>();
+        private static readonly Server1 instance = new Server1();
+
+        private Server1()
+        {
+
+        }
+
+        public static Server1 Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public bool AddServer(string address)
+        {
+            if (!address.Contains("https") && !address.Contains("http"))
+            {
+                return false;
+            }
+            else
+            {
+                if (server_list.Contains(address))
+                {
+                    return false;
+                }
+                else
+                {
+                    server_list.Add(address);
+                    return true;
+                }
+            }
+        }
+
+        public void ShowAllHTTPS()
+        {
+            if (server_list.Count == 0)
+            {
+                Console.WriteLine("Список пуст");
+                return;
+            }
+            foreach (string server in server_list)
+            {
+                if (server.StartsWith("https"))
+                    Console.Write(server + " ");
+                else Console.Write("Отсувствует(ют) адрес(ы) с https");
+            }
+            Console.WriteLine('\n');
+        }
+
+        public void ShowAllHTTP()
+        {
+            if (server_list.Count == 0)
+            {
+                Console.WriteLine("Список пуст");
+                return;
+            }
+            foreach (string server in server_list)
+            {
+                if (server.StartsWith("http") && !server.StartsWith("https"))
+                    Console.Write(server + " ");
+                else
+                {
+                    Console.Write("Отсувствует(ют) адрес(ы) с http");
+                }
+            }
+            Console.WriteLine('\n');
+        }
+    } 
+}
